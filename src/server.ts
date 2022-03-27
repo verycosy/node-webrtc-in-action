885d766c4aefe24a5e2363787e549ce71f49013d
verycosy <verycosy@kakao.com>
2022-03-28 00:18:15 +0900

    opencv sample

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,27 @@
+import * as cv from 'opencv4nodejs';
 import net from 'net';
 import { WebRtcClient } from './WebRtcClient';
+
+async function test() {
+  const wCap = new cv.VideoCapture(0);
+
+  while (1) {
+    const frame = wCap.read();
+    cv.imshow('Mirror', frame.flip(1).bgrToGray());
+
+    const key = cv.waitKey(10);
+
+    // NOTE: ESC
+    if (key === 27) {
+      wCap.release();
+      cv.destroyAllWindows();
+      cv.waitKey(1);
+      break;
+    }
+  }
+}
+
+test();
 
 const server = net
   .createServer((socket) => {
